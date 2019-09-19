72fbc7fdab63ad417a0f6b928725c4faf6fc1003
William Culhane <sxroka@gmail.com>
2019-09-18 19:23:30 -0600

    Add ability to connect to MPD

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #[derive(Debug)]
 pub struct Mpd {
-    connection: std::net::TcpStream,
+    pub connection: std::net::TcpStream,
 }
 
 impl Mpd {
     pub fn new(address: std::net::SocketAddr) -> Result<Mpd, &'static str> {
         if let Ok(stream) = std::net::TcpStream::connect(address) {
+            // TODO: Validate MPD response
             Ok(Mpd { connection: stream })
         } else {
             return Err("Failed to connect to MPD!");
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+
 fn main() {
-    let mpd = gnaw::Mpd::new("127.0.0.1:6600".parse().unwrap());
-    println!("{:#?}", mpd);
+    if let Ok(mut mpd) = gnaw::Mpd::new("127.0.0.1:6600".parse().unwrap()) {
+        let mut reader = BufReader::new(mpd.connection);
+        let mut buffer = String::new();
+        if let Ok(_) = reader.read_line(&mut buffer) {
+            println!("Buffer: {:#?}", buffer)
+        } else{
+            panic!("Failed to read version information from MPD!")
+        }
+    } else {
+        panic!("Failed to connect to mpd!")
+    }
 }
