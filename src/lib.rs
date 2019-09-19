c37acf44348e85dda311aa279eaa638ef7f46190
William Culhane <sxroka@gmail.com>
2019-09-18 20:53:10 -0600

    Moved code to library, add version to Mpd struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,32 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+
 #[derive(Debug)]
 pub struct Mpd {
     pub connection: std::net::TcpStream,
+    pub version: String,
 }
 
 impl Mpd {
     pub fn new(address: std::net::SocketAddr) -> Result<Mpd, &'static str> {
         if let Ok(stream) = std::net::TcpStream::connect(address) {
-            // TODO: Validate MPD response
-            Ok(Mpd { connection: stream })
+            let mut reader = BufReader::new(&stream);
+            let mut buffer = String::new();
+            if let Ok(_) = reader.read_line(&mut buffer) {
+                if &buffer[0..6] == "OK MPD" {
+                    buffer.pop();
+                    Ok(Mpd {
+                        connection: stream,
+                        version: buffer[7..].to_string(),
+                    })
+                } else {
+                    return Err("MPD returned an inappropriate response");
+                }
+            } else {
+                return Err("Failed to read version information from MPD");
+            }
         } else {
-            return Err("Failed to connect to MPD!");
+            return Err("Failed to connect to MPD");
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use std::io::prelude::*;
-use std::io::BufReader;
-
 fn main() {
-    if let Ok(mut mpd) = gnaw::Mpd::new("127.0.0.1:6600".parse().unwrap()) {
-        let mut reader = BufReader::new(mpd.connection);
-        let mut buffer = String::new();
-        if let Ok(_) = reader.read_line(&mut buffer) {
-            println!("Buffer: {:#?}", buffer)
-        } else{
-            panic!("Failed to read version information from MPD!")
-        }
+    if let Ok(mpd) = gnaw::Mpd::new("127.0.0.1:6600".parse().unwrap()) {
+        println!("{:#?}", mpd);
     } else {
         panic!("Failed to connect to mpd!")
     }
