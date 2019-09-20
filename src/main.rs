82159de21a7c8eeb11f919c53156e9e9649b598b
William Culhane <sxroka@gmail.com>
2019-09-20 17:08:38 -0600

    Update tab block

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::sync::mpsc;
 use std::time::Duration;
 use std::{io, thread};
-use termion::event::{Event, Key, MouseEvent};
+use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Layout};
 use tui::style::{Color, Style};
 use tui::widgets::{Block, Borders, Tabs, Widget};
 use tui::Terminal;
@@ -26,7 +27,6 @@ fn main() -> Result<(), io::Error> {
             titles: vec!["Currently Playing", "Queue", "Albums", "Artists"],
             index: 0,
         };
-        println!("{:#?}", mpd);
         let stdout = io::stdout().into_raw_mode()?;
         let backend = TermionBackend::new(stdout);
         let mut terminal = Terminal::new(backend)?;
@@ -40,8 +40,23 @@ fn main() -> Result<(), io::Error> {
                 tx.send(evt).unwrap();
             }
         });
-        'main: loop {
-            match rx.recv_timeout(Duration::from_millis(200)) {
+        loop {
+            terminal
+                .draw(|mut term| {
+                    let chunks = Layout::default()
+                        .constraints([Constraint::Min(0), Constraint::Length(3)].as_ref())
+                        .split(term.size());
+                    Tabs::default()
+                        .block(Block::default().borders(Borders::ALL))
+                        .titles(&tabs.titles)
+                        .select(tabs.index)
+                        .style(Style::default().fg(Color::White))
+                        .highlight_style(Style::default().fg(Color::Green))
+                        .divider(tui::symbols::line::VERTICAL)
+                        .render(&mut term, chunks[1]);
+                })
+            .unwrap();
+            match rx.recv_timeout(Duration::from_millis(1000)) {
                 Ok(Event::Key(Key::Char('1'))) => tabs.goto(0),
                 Ok(Event::Key(Key::Char('2'))) => tabs.goto(1),
                 Ok(Event::Key(Key::Char('3'))) => tabs.goto(2),
@@ -51,19 +66,6 @@ fn main() -> Result<(), io::Error> {
                 Err(mpsc::RecvTimeoutError::Disconnected) => panic!("isolated from input thread"),
                 Err(mpsc::RecvTimeoutError::Timeout) => {}
             }
-            terminal
-                .draw(|mut term| {
-                    let size = term.size();
-                    Tabs::default()
-                        .block(Block::default().title("Tabs").borders(Borders::ALL))
-                        .titles(&tabs.titles)
-                        .select(tabs.index)
-                        .style(Style::default().fg(Color::White))
-                        .highlight_style(Style::default().fg(Color::Yellow))
-                        .divider(tui::symbols::DOT)
-                        .render(&mut term, size);
-                })
-                .unwrap();
         }
         terminal.show_cursor().unwrap();
         terminal.clear()?;
