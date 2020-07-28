cd214cda17336c8c496acac11d374e39c62a1c70
William Culhane <sxroka@gmail.com>
2020-07-27 23:38:47 -0500

    Implement basic error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::HashMap;
+use std::error::Error;
+use std::fmt;
 use std::io::{prelude::*, BufReader};
 use std::net::{SocketAddr, TcpStream};
 
@@ -10,6 +12,7 @@ pub struct Mpd {
     pub version: String,
 }
 
+// TODO Implement error trait
 #[derive(Debug)]
 pub struct MpdError {
     code: usize,
@@ -20,6 +23,16 @@ pub struct MpdError {
 
 type Song<'a> = HashMap<String, String>;
 
+impl fmt::Display for MpdError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "code: {}, position: {}, command: {}, message: {}",
+            self.code, self.list_position, self.current_command, self.message_text
+        )
+    }
+}
+
 impl Mpd {
     fn handle_error(response: &str) -> MpdError {
         lazy_static! {
@@ -29,12 +42,15 @@ impl Mpd {
 
         let caps = MPD_ERROR_REGEX.captures(response).unwrap();
         MpdError {
+            // I can't tell if this is beautiful or ugly
+            // It's probably not good
             code: caps.get(1).unwrap().as_str().parse().unwrap(),
             list_position: caps.get(2).unwrap().as_str().parse().unwrap(),
             current_command: caps.get(3).unwrap().as_str().to_string(),
             message_text: caps.get(4).unwrap().as_str().to_string(),
         }
     }
+    // TODO Implement MpdError
     pub fn new(address: SocketAddr) -> Result<Mpd, &'static str> {
         if let Ok(stream) = TcpStream::connect(address) {
             let mut reader = BufReader::new(&stream);
@@ -55,6 +71,7 @@ impl Mpd {
             Err("failed to connect to MPD")
         }
     }
+    // TODO Implement MpdError
     pub fn current_song(&mut self) -> Result<Song, &'static str> {
         self.connection
             .write(b"currentsong\n")
@@ -76,6 +93,7 @@ impl Mpd {
             Ok(current_song)
         }
     }
+    // TODO Implement MpdError
     pub fn status(&mut self) -> Result<HashMap<String, String>, &'static str> {
         self.connection
             .write(b"status\n")
@@ -93,7 +111,7 @@ impl Mpd {
         }
         Ok(status)
     }
-    pub fn stats(&mut self) -> Result<HashMap<String, String>, &'static str> {
+    pub fn stats(&mut self) -> Result<HashMap<String, String>, MpdError> {
         self.connection
             .write(b"stats\n")
             .expect("failed to write to MPD connection");
@@ -103,13 +121,16 @@ impl Mpd {
         for line in reader.lines().map(|l| l.unwrap()) {
             if line == "OK" {
                 break;
+            } else if &line[0..3] == "ACK" {
+                // Fragile slice
+                return Err(Mpd::handle_error(&line));
             }
             let split: std::vec::Vec<&str> = line.split(":").collect();
             stats.insert(split[0].to_string(), split[1][1..].to_string());
         }
         Ok(stats)
     }
-    pub fn clear_error(&mut self) {
+    pub fn clear_error(&mut self) -> Result<(), MpdError> {
         self.connection
             .write(b"clearerror\n")
             .expect("failed to write to MPD connection");
@@ -119,11 +140,10 @@ impl Mpd {
             .read_to_string(&mut response)
             .expect("failed to read from MPD connection");
 
-        // TODO Handle failure response
         if response == "OK" {
-            return;
+            Ok(())
         } else {
-            panic!("MPD returned invalid response on clearerror");
+            Err(Mpd::handle_error(&response))
         }
     }
 }
